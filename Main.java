f5251dc1cd38d12f7bf9f0366aae4d490eb0ea61
RaineVil-GH <velibaldan@hotmail.de>
2025-12-16 11:43:36 +0100

    Add files via upload
    HIer ist die neue GUI package und habe in main die gui aufrufen lassen.

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-import com.sun.jdi.event.StepEvent;
+
 import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
 import de.tudresden.sumo.cmd.Trafficlight;
-import trafficparticipants.Car;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String SUMO_BIN = "\\home\\abdulrehman\\sumo\\bin\\sumo-gui.exe";
-        String SUMO_CFG = "\\home\\abdulrehman\\Downloads\\Text-Java\\TestM2.sumocfg";
+        String SUMO_BIN = "C:\\Program Files (x86)\\Eclipse\\Sumo\\bin\\sumo-gui.exe";
+        String SUMO_CFG = "C:\\Users\\delil\\Java_SUMO_Projekt_Final\\TestM2.sumocfg";
 
         SumoTraciConnection conn = new SumoTraciConnection(SUMO_BIN, SUMO_CFG);
         conn.runServer();
@@ -18,35 +21,44 @@ public class Main {
         List<String> TrafficLightsIds = (List<String>) conn.do_job_get(Trafficlight.getIDList());
         System.out.println(TrafficLightsIds + "\nTrafficLights: " + TrafficLightsIds.size());
 
-        List<String> CarIds = (List<String>) conn.do_job_get(Vehicle.getIDList());
-        System.out.println(CarIds + "\nCars: " + TrafficLightsIds.size());
+
+        Car c1 = new Car("c");
+        c1.createCar(10, conn);
+
+        Pedestrian p1 = new Pedestrian("p");
+        p1.createPedestrian(10, conn);
+
+        Motorcycle m1 = new Motorcycle("m");
+        m1.createMotorcycle(10, conn);
+
+        Cyclist cc1 = new Cyclist("cc");
+        cc1.createCyclist(10, conn);
+
+        Bus b1 = new Bus("b");
+        b1.createBus(10, conn);
+
 
         List<TrafficlightModul> trafficlights = new ArrayList<>();
-        for(String id : TrafficLightsIds){
+        for(String id : TrafficLightsIds) {
             TrafficlightModul tl = new TrafficlightModul(id);
             trafficlights.add(tl);
         }
-        for (int i = 0; i < 500 ; i++)
-        {
-            for (int j = 0; j < TrafficLightsIds.size(); j++) {
-                String s = TrafficLightsIds.get(j);
-                System.out.println("Trafficlight " + (j+1) + ": " + conn.do_job_get(Trafficlight.getRedYellowGreenState(s)));
-            }
-            conn.do_timestep();
-            System.out.println("Step: " + (i+1));
+        trafficlights.get(0).setMultiPhase(conn,"GGGGGGGGGGGG");
+        
+        //Starting the GUI and the Visualisation
+		new GuiSumoBridge().startGui();
+        
+        Steps s1 = new Steps(conn, TrafficLightsIds);
+        s1.StepCounter();
+       
+        //Simulating the trafficlights and cars
+        new GuiSumoBridge().ongoingGui();
+        
+        synchronized (LOCK.CONN_LOCK) {
+            List<String> CarIds = (List<String>) conn.do_job_get(Vehicle.getIDList());
+            System.out.println(CarIds + "\nVehicles: " + CarIds.size());
+
         }
-
-        trafficlights.get(0).setMultiPhase(conn,"GRrRRgGrrrrr");
-
-
-        Steps Zaehler = new Steps(conn);
-        Zaehler.StepCounter();
-
-
-
-
-
+			
     }
-
 }
-
