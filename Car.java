6f2882c34b65e2af848105ac16fc97d854000d10
Melihcan38 <melihcan.kocaturk@yahoo.com>
2026-01-08 19:24:12 +0100

    Enhance createCar method with optional STD_ID
    Refactor createCar method to support optional STD_ID parameter.

--- Car.java
+++ Car.java
@@ -1,3 +1,4 @@
+package src;
 import de.tudresden.sumo.cmd.Simulation;
 import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
@@ -6,7 +7,7 @@ public class Car extends VehicleModul {
 
 
     public int depart;
-    double position;
+    public double position;
     public Car (String id)
     {
         super(id);
@@ -18,9 +19,15 @@ public class Car extends VehicleModul {
         this.departLane = (byte) -2;
     }
     int count = 0;
-    public void createCar(int amount, SumoTraciConnection conn) throws Exception {
+
+    public void createCar(int amount, SumoTraciConnection conn) throws Exception
+    {
+        createCar(amount, conn, -1);
+    }
+
+    public void createCar(int amount, SumoTraciConnection conn, int STD_ID) throws Exception {
         for(int i = 0; i < amount; i++) {
-            String id2 = "Car" + count;
+            String id2 = STD_ID + "Car" + count;
             count++;
             Car c = new Car(id2);
             synchronized (LOCK.CONN_LOCK) {
@@ -28,18 +35,4 @@ public class Car extends VehicleModul {
             }
         }
     }
-
-    /*public void setCarSpeedLow(SumoTraciConnection conn) throws Exception {
-        setSpeedINSumo(conn, 50);
-    }
-
-    public void setCarSpeedMedium(SumoTraciConnection conn) throws Exception {
-        setSpeedINSumo(conn, 100);
-    }
-
-
-    public void setCarSpeedHigh(SumoTraciConnection conn) throws Exception {
-        setSpeedINSumo(conn, 150);
-    }*/
-
 }
