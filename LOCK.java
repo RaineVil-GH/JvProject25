261745d7690afa8d162c716e4b160b87e8a1782e
Melihcan38 <melihcan.kocaturk@yahoo.com>
2026-01-08 19:36:43 +0100

    Enhance LOCK class with ID_LOCK and nextId method
    Added an AtomicInteger for ID locking and a method to get the next ID.

--- LOCK.java
+++ LOCK.java
@@ -1,3 +1,13 @@
+package src;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class LOCK {
     public static final Object CONN_LOCK = new Object();
+    public static final AtomicInteger ID_LOCK = new AtomicInteger(0);
+
+    public static int nextId(){
+        return ID_LOCK.incrementAndGet();
+    }
+
 }
