a2fca6867335219bdf1bece5a5d6ffde4776fbac
Melihcan38 <melihcan.kocaturk@yahoo.com>
2026-01-08 19:37:48 +0100

    Enhance createMotorcycle with optional STD_ID
    Refactor createMotorcycle method to include optional STD_ID parameter for better flexibility in ID generation.

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,3 +1,4 @@
+package src;
 import de.tudresden.sumo.cmd.Simulation;
 import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
@@ -16,28 +17,21 @@ public class Motorcycle extends VehicleModul {
     }
 
     int count = 0;
-    public void createMotorcycle(int amount, SumoTraciConnection conn) throws Exception {
+
+    public void createMotorcycle(int amount, SumoTraciConnection conn) throws Exception
+    {
+        createMotorcycle(amount, conn, -1);
+    }
+
+    public void createMotorcycle(int amount, SumoTraciConnection conn, int STD_ID) throws Exception {
         for(int i = 0; i < amount; i++) {
-            String id2 = "Motorcycle" + count;
+            String id2 = STD_ID +  "Motorcycle" + count;
             count++;
             Motorcycle m = new Motorcycle(id2);
             synchronized (LOCK.CONN_LOCK) {
                 conn.do_job_set(Vehicle.add(m.id, m.type, m.routeID, m.depart, m.position, m.speed, m.departLane));
             }
         }
-    }
-
-    public void setMotorcycleSpeedLow(SumoTraciConnection conn) throws Exception {
-        setSpeedINSumo(conn, 30);
-    }
-
-    public void setMotorcycleSpeedMedium(SumoTraciConnection conn) throws Exception {
-        setSpeedINSumo(conn, 100);
-    }
-
-
-    public void setMotorcycleSpeedHigh(SumoTraciConnection conn) throws Exception {
-        setSpeedINSumo(conn, 150);
     }
 
 }
