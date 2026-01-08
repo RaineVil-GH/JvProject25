df6871027c92a962232b775582f394a2110ab37a
Melihcan38 <melihcan.kocaturk@yahoo.com>
2026-01-08 19:33:10 +0100

    Refactor createCyclist method to include STD_ID

--- Cyclist.java
+++ Cyclist.java
@@ -1,3 +1,4 @@
+package src;
 import de.tudresden.sumo.cmd.Simulation;
 import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
@@ -16,18 +17,20 @@ public class Cyclist extends VehicleModul {
     }
 
     int count = 0;
-    public void createCyclist(int amount, SumoTraciConnection conn) throws Exception {
+
+    public void createCyclist(int amount, SumoTraciConnection conn) throws Exception
+    {
+        createCyclist(amount, conn, -1);
+    }
+
+    public void createCyclist(int amount, SumoTraciConnection conn, int STD_ID) throws Exception {
         for(int i = 0; i < amount; i++) {
-            String id2 = "Cyclist" + count;
+            String id2 = STD_ID + "Cyclist" + count;
             count++;
             Cyclist cc = new Cyclist(id2);
             synchronized (LOCK.CONN_LOCK) {
                 conn.do_job_set(Vehicle.add(cc.id, cc.type, cc.routeID, cc.depart , cc.position, cc.speed, cc.departLane));
             }
         }
-    }
-
-    public void setCyclistSpeed(SumoTraciConnection conn) throws Exception{
-        setSpeedINSumo(conn, 5.0);
     }
 }
