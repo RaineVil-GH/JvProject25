60688336037f19f3287f80a822c524b2cd279cf9
Delil04 <kb2r7p47rj@privaterelay.appleid.com>
2026-01-15 18:30:04 +0100

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,11 +1,19 @@
 package src;
 import de.tudresden.sumo.cmd.Simulation;
+import de.tudresden.sumo.cmd.Trafficlight;
 import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+
+import java.awt.*;
 
 public class Car extends VehicleModul {
 
 
+    public static List<String> carIds = Collections.synchronizedList(new ArrayList<>());
     public int depart;
     public double position;
     public Car (String id)
@@ -18,21 +26,33 @@ public class Car extends VehicleModul {
         this.position = 0.0;
         this.departLane = (byte) -2;
     }
-    int count = 0;
 
-    public void createCar(int amount, SumoTraciConnection conn) throws Exception
-    {
-        createCar(amount, conn, -1);
-    }
+    static int count = 0;
+    public void createCar(int amount, SumoTraciConnection conn, String route, String color) throws Exception {
+        Thread cars = new Thread(() -> {
+            try
+            {
+                for(int i = 0; i < amount; i++) {
+                    String id2 = "Car" + count;
+                    count++;
+                    Car c = new Car(id2);
+                    c.routeID = route;
 
-    public void createCar(int amount, SumoTraciConnection conn, int STD_ID) throws Exception {
-        for(int i = 0; i < amount; i++) {
-            String id2 = STD_ID + "Car" + count;
-            count++;
-            Car c = new Car(id2);
-            synchronized (LOCK.CONN_LOCK) {
-                conn.do_job_set(Vehicle.add(c.id, c.type, c.routeID, c.depart , c.position, c.speed, c.departLane));
+                    synchronized (LOCK.CONN_LOCK) {
+                        conn.do_job_set(Vehicle.add(c.id, c.type, c.routeID, c.depart , c.position, c.speed, c.departLane));
+                        conn.do_job_set(Vehicle.setColor(c.id, VehicleModul.getColor(color)));
+                    }
+                    Car.carIds.add(c.id);
+                }
+
             }
-        }
+            catch  (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        cars.start();
     }
+
 }
