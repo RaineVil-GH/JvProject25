510e0502152b985270a344c79b114f5d58eae2f3
Delil04 <kb2r7p47rj@privaterelay.appleid.com>
2026-01-15 18:32:12 +0100

    Update Motorcycle.java

--- Motorcycle.java
+++ Motorcycle.java
@@ -4,34 +4,53 @@ import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
 public class Motorcycle extends VehicleModul {
 
-    public Motorcycle (String id)
-    {
+    public Motorcycle(String id) {
         super(id);
         this.type = "t_3";
         this.routeID = "r_3";
         this.speed = 15.0;
         this.depart = 0;
         this.position = 0.0;
-        this.departLane = (byte) 2;
+        this.departLane = (byte) -2;
 
     }
 
-    int count = 0;
+    static int count = 0;
+    public void createMotorcycle(int amount, SumoTraciConnection conn, String route, String color) throws Exception {
+        Thread motorcycle = new Thread(() -> {
+            try {
+                for (int i = 0; i < amount; i++) {
+                    String id2 = "Motorcycle" + count;
+                    count++;
+                    Motorcycle m = new Motorcycle(id2);
+                    m.routeID = route;
 
-    public void createMotorcycle(int amount, SumoTraciConnection conn) throws Exception
-    {
-        createMotorcycle(amount, conn, -1);
-    }
+                    synchronized (LOCK.CONN_LOCK) {
+                        conn.do_job_set(Vehicle.add(m.id, m.type, m.routeID, m.depart, m.position, m.speed, m.departLane));
+                        conn.do_job_set(Vehicle.setColor(m.id, VehicleModul.getColor(color)));
+                    }
+                }
 
-    public void createMotorcycle(int amount, SumoTraciConnection conn, int STD_ID) throws Exception {
-        for(int i = 0; i < amount; i++) {
-            String id2 = STD_ID +  "Motorcycle" + count;
-            count++;
-            Motorcycle m = new Motorcycle(id2);
-            synchronized (LOCK.CONN_LOCK) {
-                conn.do_job_set(Vehicle.add(m.id, m.type, m.routeID, m.depart, m.position, m.speed, m.departLane));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
+
+        });
+        motorcycle.start();
     }
+
+    public void setMotorcycleSpeedLow(SumoTraciConnection conn) throws Exception {
+        setSpeedINSumo(conn, 30);
+    }
+
+    public void setMotorcycleSpeedMedium(SumoTraciConnection conn) throws Exception {
+        setSpeedINSumo(conn, 100);
+
+    }
+
+    public void setMotorcycleSpeedHigh(SumoTraciConnection conn) throws Exception {
+        setSpeedINSumo(conn, 150);
+    }
+
 
 }
